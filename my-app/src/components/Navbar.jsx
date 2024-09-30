1ce34a2424fee7e8f19be817a2a3925385fa3d50
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-30 07:35:53 +0300

    almond-blossoms

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 flex justify-center py-4 shadow-lg">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 justify-center items-center max-w-screen-lg w-full px-4">
+    <nav className="fixed bottom-0 left-0 right-0 flex justify-center py-4 shadow-lg mx-auto">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4 justify-center items-center max-w-screen-lg w-full px-4 mx-auto">
         <Link
           to="/"
           className="text-black text-center bg-white shadow-md p-4 rounded-md"
@@ -27,6 +27,12 @@ const Navbar = () => {
           className="text-black text-center bg-white shadow-md p-4 rounded-md"
         >
           <p className="font-bold">Irises</p>
+        </Link>
+        <Link
+          to="/almond-blossoms"
+          className="text-black text-center bg-white shadow-md p-4 rounded-md"
+        >
+          <p className="font-bold">Almond Blossoms</p>
         </Link>
 
       </div>
